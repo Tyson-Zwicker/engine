40835fcb211580fa052a964ca8d15f10a6197e43
Tyson-Zwicker <tysonzwicker@gmail.com>
2024-12-21 10:22:12 -0400

    add rad<->deg functions

--- maths.js
+++ maths.js
@@ -11,7 +11,12 @@ const Point = function (x, y) {
     this.x = x;
     this.y = y;
 }
-
+const rad = function (a){
+    return Math.PI*a/180;
+}
+const ang = function (r){
+    return a*10/Math.PI;
+}
 const length = function (x, y) {
     return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
