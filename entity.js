75d3e4a2ff62b55380fd47158bbe6a933fae9df0
Tyson-Zwicker <tysonzwicker@gmail.com>
2024-12-22 00:52:48 -0400

    started adding >1 sprite..

--- entity.js
+++ entity.js
@@ -1,25 +1,25 @@
 /* 
     An Entity knows, where it is, how fast its moving, where its 
     facing, how quickly its spinning, and what it looks like (in
-    the form of a sprite.)
+    the form of a collection sprites.)
     
     It has functions to do update the position and facing,
-    change the velocity and draw the sprite.
+    change the velocity and draw the sprites.
 */
-const Entity = new function (name, sprite, position, angle, velocity, spin, mass) {
+const Entity = function (name, sprites, position, angle, velocity, spin, mass) {
     this.name = name;
     this.position = position;
     this.angle = angle;
     this.velocity = velocity;
     this.spin = spin;
-    this.sprite = sprite;
+    this.sprites = sprites;
     this.mass = mass;
 }
 
 Entity.prototype.move = function (delta) {
     this.angle += this.spin;
     this.position.x += (this.velocity.x / delta);
-    this.position.y += (this.velcotity.y / delta);
+    this.position.y += (this.velocity.y / delta);
 }
 
 Entity.prototype.push = function (vector) {
@@ -28,8 +28,10 @@ Entity.prototype.push = function (vector) {
 }
 Entity.prototype.draw = function (camera, scale) {
     spriteLocation = {
-        x: (this.position.x - camera.position.x) / scale + canvas.width / 2,
-        y: (this.position.y - camera.position.y) / scale + canvas.height / 2
+        x: (this.position.x - camera.x) / scale + canvas.width / 2,
+        y: (this.position.y - camera.y) / scale + canvas.height / 2
     };
-    this.sprite.draw(scale, this.angle, spriteLocation);
+    this.sprites.forEach ((s)=>{
+        s.draw(scale, this.angle, spriteLocation);
+    });
 }
