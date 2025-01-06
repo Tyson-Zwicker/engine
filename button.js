26e32fe3749d0013491266e215bca53dcccf3880
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-05 20:15:04 -0400

    all good 20040105

--- button.js
+++ button.js
@@ -7,19 +7,13 @@ const Button = function (name, text, x0, y0, x1, y1, fontsize, color, bgColor) {
     this.y1 = y1;
     this.color = color;
     this.bgColor = bgColor;
-   
     this.width = x1 - x0;
     this.height = y1 - y0;
     this.fontsize = fontsize;
     this.active = false;
-
 }
 Button.prototype.draw = function () {
-
     //Invert the colors if active.
-
-   
-
     if (!this.active) {
         ctx.strokeStyle = this.color;
         ctx.fillStyle = this.bgColor;
@@ -27,34 +21,24 @@ Button.prototype.draw = function () {
         ctx.strokeStyle = this.bgColor;
         ctx.fillStyle = this.color;
     }
-
     ctx.strokeWidth = 2;
-
     /* Rectangle */
     ctx.fillRect(this.x0, this.y0, this.width, this.height);    
     ctx.strokeRect(this.x0, this.y0, this.width, this.height);
-
     /* center the text*/
     ctx.font = `${this.fontsize}em monospace`;
     let measure = ctx.measureText(this.text);
-
     let w = measure.actualBoundingBoxRight +
         measure.actualBoundingBoxLeft;
     let h = measure.fontBoundingBoxDescent +
         measure.fontBoundingBoxAscent;
     let tx = this.x0 + (this.width - w) / 2;
     let ty = this.y1 - (this.height - h) / 2;
-
-    
-
     if (this.active) {
         ctx.fillStyle = this.bgColor;
     } else {
         ctx.fillStyle = this.color
 
     }
-  
-
-
     ctx.fillText(this.text + '*', tx, ty);
 }
