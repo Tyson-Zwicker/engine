f0fc738203f97fe1c1d577ed5d7e099f97d106df
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-07 20:29:12 -0400

    fixed line borders

--- gfx.js
+++ gfx.js
@@ -1,4 +1,4 @@
-const drawBox = function (x0, y0, x1, y1, color, fill) {
+const drawBox = function (x0, y0, x1, y1, color, fill, thickness) {
     ctx.beginPath();
     if (fill) {
         ctx.fillStyle = color;
@@ -6,27 +6,34 @@ const drawBox = function (x0, y0, x1, y1, color, fill) {
     } else {
         ctx.strokeWidth = 1;
         ctx.strokeStyle = color;
+        ctx.lineWidth = (thickness) ? thickness : 1;
         ctx.strokeRect(x0, y0, x1 - x0, y1 - y0);
     }
     ctx.closePath();
 }
-const drawArc = function (x, y, r, a0, a1, color) {
+const drawArc = function (x, y, r, a0, a1, color, thickness) {
     ctx.beginPath();
     ctx.strokeWidth = 1;
     ctx.strokeStyle = color;
+    ctx.lineWidth = (thickness) ? thickness : 1;
     ctx.arc(x, y, r, a0, a1);
     ctx.stroke();
 }
-const drawCircle = function (x, y, r, color, fill) {
+const drawCircle = function (x, y, r, color, fill, thickness) {
+    ctx.lineWidth = (thickness) ? thickness : 1;
     drawArc(x, y, r, 0, TAU, color);
+    if (fill) {
+        ctx.fill();
+    }
 }
-const drawPie = function (x, y, r, a0, a1, color, fill) {
+const drawPie = function (x, y, r, a0, a1, color, fill, thickness) {
+    ctx.lineWidth = (thickness) ? thickness : 1;
     let x0 = x + cos(a0) * r;
     let y0 = y + sin(a0) * r;
     let x1 = x + cos(a1) * r;
     let y1 = y + sin(a1) * r;
     ctx.beginPath();
-    ctx.strokeWidth = 1;
+    ctx.lineWidth = 1;
     ctx.strokeStyle = color;
     ctx.moveTo(x, y);
     ctx.lineTo(x0, y0);
@@ -42,7 +49,7 @@ const drawPie = function (x, y, r, a0, a1, color, fill) {
 const drawPixel = function (x, y, color) {
     let previousSize = ctx.strokeWidth;
     let strokeStyle = 1;
-    drawBox(x - 0.5, y - 0.5, x + 0.5, y + 0.5, color);
+    drawBox(x, y, x + 1, y + 1, color);
     ctx.strokeWidth = previousSize;
 }
 const drawLine = function (x0, y0, x1, y1, thickness, color) {
@@ -55,23 +62,23 @@ const drawLine = function (x0, y0, x1, y1, thickness, color) {
     ctx.stroke();
 }
 const drawTextCenter = function (x, y, text, size, color) {
-    ctx.font = (size)? `${size}em monospace`: '0.75em monospace';
+    ctx.font = (size) ? `${size}em monospace` : '0.75em monospace';
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.fillStyle = (color) ? color : '#ccc';
-    ctx.fillText(text,x, y);
+    ctx.fillText(text, x, y);
 }
 const drawTextLeft = function (x, y, text, size, color) {
-    ctx.font = (size)? `${size}em monospace`: '0.75em monospace';
+    ctx.font = (size) ? `${size}em monospace` : '0.75em monospace';
     ctx.textBaseline = "middle";
     ctx.textAlign = "start";
     ctx.fillStyle = (color) ? color : '#ccc';
-    ctx.fillText(text,x, y);
+    ctx.fillText(text, x, y);
 }
 const drawTextRight = function (x, y, text, size, color) {
-    ctx.font = (size)? `${size}em monospace`: '0.75em monospace';
+    ctx.font = (size) ? `${size}em monospace` : '0.75em monospace';
     ctx.textBaseline = "middle";
     ctx.textAlign = "end";
     ctx.fillStyle = (color) ? color : '#ccc';
-    ctx.fillText(text,x, y);
+    ctx.fillText(text, x, y);
 }
