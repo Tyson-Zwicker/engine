9d0b9a0bf58d4d98254a68648f32f761a54348fd
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-13 12:25:55 -0400

    mousewheel zoom revealed issues..fixed

--- entitylabel.js
+++ entitylabel.js
@@ -4,29 +4,22 @@ const EntityLabel = function (entity, fontSize, color) {
     this.color = color;
 }
 
-EntityLabel.prototype.draw = function (text, camera, scale) {
-    //So we're drawing some text, under the entity, unless its at the bottom of the screen, then it goes on top..
-
+EntityLabel.prototype.draw = function (text) {
     let rowSize = this.fontSize * 16 + 2;
     let height = rowSize * text.length;
-   
-    /*
-    let width =0;
-    text.forEach (line =>{
-        lineWidth = getTextWidth (line, `${this.fontSize}em monospace`);
-        width = (lineWidth>width)? lineWidth: width; 
-    });
-    */
-
-    //determine y based on closeness of edge..   
-    let x = (this.entity.position.x - camera.x) * scale + centerOfScreen.x;
-    let y = (this.entity.position.y - camera.y) * scale + centerOfScreen.y + this.entity.radius * scale;
+    let x = (this.entity.position.x - camera.x) * zoom + centerOfScreen.x;
+    let y = (this.entity.position.y - camera.y) * zoom + centerOfScreen.y + this.entity.radius * zoom;
     let drawDirection = 1;
-    if (y > screenSize.y) {
-        y = (this.entity.position.y - camera.y) * scale + centerOfScreen.y - this.entity.radius * scale;
+    let textToDraw = text;
+    /*If the sprite is below the bottom the screen, show the label on top instead of below, and reverse the
+     order in which the text is drawn */
+    if (y + rowSize * text.length > screenSize.y) {
+        y = (this.entity.position.y - camera.y) * zoom + centerOfScreen.y - this.entity.radius * zoom;
         drawDirection = -1;
+        textToDraw = text.toReversed();
     }
-    text.forEach (line =>{
+    
+    textToDraw.forEach (line =>{
         drawTextCenter(x, y, line, this.fontSize);        
         y += (rowSize * drawDirection);
     });
