d3c5bdec02e41a196ad4195ff6e3ceb501e04c9c
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-13 21:05:43 -0400

    This is the whole point of the project. It will hide all the messy stuff and provide some convient/succint and consistent methods to deal with it, and also do all the updates and drawing in one method calls.

--- engine.js
+++ engine.js
@@ -2,13 +2,20 @@
 const Engine = function (){
     let buttonManager = new ButtonManager ();
     let entityManager = new EntityManager ();
-    
+    //tattler..
+    //particle engine..
 }
 //
 Engine.prototype.addButton = function (name, text, x0, y0, x1, y1, fontsize, color, bgColor, hColor, hbgColor, action){
     ButtonManager.addButton (new Button (name, text, x0, y0, x1, y1, fontsize, color, bgColor, hColor, hbgColor, action));
 }
-
+//add a clean-out so  buttons can be reset..
 Engine.prototype.addEntity = function (name, sprites, position, angle, velocity, spin, mass, radius){
     EntityManager.addEntity (new Entity (name, sprites, position, angle, velocity, spin, mass, radius));
 }
+//add remove entity by name..
+//also remove all entries.
+//add tattle methods to hide the Tale part- just take the parameters and do that in here..
+//particle generator methods.
+
+//Then a big 'ol do() method that makes every move, draw, tell, sparkle, whatever..
