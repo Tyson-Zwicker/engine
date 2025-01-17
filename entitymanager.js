c5dc6ff019d7537073fca8a406009f346114f263
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-17 19:45:39 -0400

    the engine's going to track clicks to entities so.. changes.

--- entitymanager.js
+++ entitymanager.js
@@ -1,10 +1,13 @@
-const EntityManager = function (entities) {
+const EntityManager = function (entities, touchedFn) {
     this.entities = {};
+    this.touchedName = null;
+    this.touchedFn = touchedFn;
     if (entities) {
         entities.forEach(entity => {
             this.entities[entity.name] = entity;
         });
     }
+
 }
 EntityManager.prototype.manage = function () {
     Object.getOwnPropertyNames(this.entities).forEach(entityName => {
@@ -29,4 +32,21 @@ EntityManager.prototype.get = function (entityName) {
 }
 EntityManager.prototype.has = function (entityName) {
     return this.entities.hasOwnProperty(entityName);
+}
+EntityManager.prototype.checkTouch = function () {
+    this.touchedName = null;
+    this.entities.forEach(entity => {
+        if (touchedName === null) { //break out the heavy stuff once you find one.
+            let location = entity.getWindowLocation();
+            let radius = entity.radius / _zoom;
+            let bounds = { x0: location.x - radius, y0: location.y - radius, x1: location.x + radius, y1: location.y + radius };
+            if (_mouse.buttonDown) {
+                if (bounded(_mouse.move.down.where, bounds)) {
+                    this.touchedName = entity.Name;
+                    this.touchedFn (touchedName);
+                }
+            }
+        }
+    });
+    return touchedName;
 }
