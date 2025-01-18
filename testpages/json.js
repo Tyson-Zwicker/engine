1e4a3dbaa44cfb1597e8e18817bc5d6e539796e9
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-18 12:08:21 -0400

    I'm about to make everything serializable. I wanted to look a the JSON.  I think serialization will be centralized because I'm going to need to manually add inner functions and assign a prototype.

---
+++ testpages/json.js
@@ -0,0 +1,58 @@
+
+const tabler = new Tabler();
+const body = document.getElementsByTagName('body')[0];
+function newRow(text) {
+    let row = tabler.getEmptyRowElement();
+    let cell = tabler.getEmptyDataElement();
+    row.appendChild(cell);
+    return row;
+}
+function showJSONTables (){
+    const sprite1 = new Sprite([
+        new SpriteLine(0, -150, 100, 100, '#ff0', 3),
+        new SpriteLine(100, 100, -100, 100, '#ff0', 3),
+        new SpriteLine(-100, 100, 0, -150, '#ff0', 3)
+    ]);
+
+    let spriteDiv = document.getElementById('spriteDiv');
+    let table = tabler.getEmptyTableElement('Sprite JSON');
+    let row = tabler.getEmptyRowElement();
+    let cell = tabler.getEmptyDataElement();
+    cell.innerText = JSON.stringify(hull);
+    row.appendChild(cell);
+    table.appendChild(row);
+    spriteDiv.appendChild(table);
+
+    const entity = new Entity(
+        'sampleEntity',
+        [hull, superstructure],
+        { x: 0, y: 0 },
+        rad(rnd(0, 360)),
+        { x: 3, y: -2 }
+        , rad(25), 100, 150
+    );
+    let entityDiv = document.getElementById('entityDiv');
+    table = tabler.getEmptyTableElement('Entity JSON');
+    row = tabler.getEmptyRowElement();
+    cell = tabler.getEmptyDataElement();
+    cell.innerText = JSON.stringify(entity);
+    row.appendChild(cell);
+    table.appendChild(row);
+    spriteDiv.appendChild(table);
+    const button = new Button(
+        'sampleButton',
+        'Look a Button!',
+        30, 40, 180, 70, 1.4,
+        '#700', '#aa0',
+        '#00f', '#0ff',
+        () => { return 'function result' }
+    );
+    let buttonDiv = document.getElementById('buttonDiv');
+    table = tabler.getEmptyTableElement('Button JSON');
+    row = tabler.getEmptyRowElement();
+    cell = tabler.getEmptyDataElement();
+    cell.innerText = JSON.stringify(button);
+    row.appendChild(cell);
+    table.appendChild(row);
+    buttonDiv.appendChild(table);
+}
