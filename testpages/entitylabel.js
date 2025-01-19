cddfb156c793a9559f0f9c11aa2350180de73786
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-18 23:30:54 -0400

    moved most of JS out of the html in the tests. Makes VSCode treat it better...

---
+++ testpages/entitylabel.js
@@ -0,0 +1,40 @@
+const entity1 = new Entity(
+    'Entity 1',
+    [hull, superstructure],
+    new Point(1500, 2500),
+    rad(45),
+    new Point(-50, -50),
+    0, 100, 150
+);
+const entity2 = new Entity(
+    'Entity 2',
+    [hull, superstructure],
+    new Point(-1600, -3100),
+    rad(90),
+    new Point(20, 20),
+    rad(10), 100, 150
+);
+const entity3 = new Entity(
+    'Entity 3',
+    [hull, superstructure],
+    new Point(0, 0),
+    rad(0),
+    new Point(0, 0),
+    rad(10), 100, 150
+);
+const redLabel = new EntityLabel(.66, '#F00');
+const greenLabel = new EntityLabel(.66, '#0F0');
+let labelContents = [
+    { name: 'name', label: '' },
+    { name: 'position', label: 'P:' },
+    { name: 'velocity', label: 'V:' }
+];
+let labelContents2 = [
+    { name: 'name', label: '' },
+    { name: 'position', label: 'P:' },
+    { name: 'velocity', label: 'V:' },
+    { name: 'angle', label: 'H:' }
+]
+entity1.assignLabel(redLabel.clone(), labelContents);
+entity2.assignLabel(redLabel.clone(), labelContents);
+entity3.assignLabel(greenLabel.clone(), labelContents2);
---
+++ testpages/entitytouch.js
@@ -0,0 +1,30 @@
+const engine = new Engine();
+//name,sprite, offset, orientation
+let fwdturret = new Part('fwdturret', turret, new Point(0, -100), 0);
+let aftturret = new Part('aftturret', turret, new Point(0, 100), rad(180));
+engine.addButton('testbutton1', 'add ent#2', 0, 180, 150, 210, 1.3, '#fff', '#600', '#007', '#fff', () => {
+    engine.addEntity("entity2", [hull, superstructure], new Point(400, 0), 0, new Point(0, 0), 0, 10, 200, []);
+});
+engine.addButton('testbutton2', 'rmv ent#2', 0, 220, 150, 250, 1.3, '#fff', '#600', '#007', '#fff', () => {
+    engine.removeEntity('entity2');
+});
+engine.addButton('testbutton3', 'add prt to ent2', 0, 260, 150, 290, 1.3, '#fff', '#600', '#007', '#fff', () => {
+    engine.addEntityPart('entity2', aftturret);
+});
+engine.addButton('testbutton4', 'rem prt frm ent2', 0, 300, 150, 330, 1.3, '#fff', '#600', '#007', '#fff', () => {
+    engine.removeEntityPart('entity2', 'aftturret');
+});
+
+engine.addTattler('1em monospace', 10, 300);
+//name, sprites, position, angle, velocity, spin, mass, radius, parts
+engine.addEntity(
+    "entity1", [hull, superstructure], new Point(0, 0), 0, new Point(0, 0), 0, 10, 200, [fwdturret, aftturret]
+);
+engine.setEntityTouchEvent((entityName) => {
+    if (entityName) {
+        engine.tellTattleGrouped('touched', entityName, '#9ff');
+    }
+});
+engine.setBackgroundTouchEvent(() => {
+    engine.tellTattleGrouped('bground', `${_mouse.move.where.x},${_mouse.move.where.y}`, '#aaa');
+});
