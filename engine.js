56c7365a07040171a7b0d19170d0b6b850a97bc2
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-21 12:13:04 -0400

    type in unToggleFn parameter of addButton()

--- engine.js
+++ engine.js
@@ -10,8 +10,8 @@ const Engine = function () {
     this.tattler = null;
     this.backgroundTouchFn = undefined;
 }
-Engine.prototype.addButton = function (name, text, x0, y0, x1, y1, fontsize, color, bgColor, hColor, hbgColor, action) {
-    this.buttonManager.addButton(new Button(name, text, x0, y0, x1, y1, fontsize, color, bgColor, hColor, hbgColor, action));
+Engine.prototype.addButton = function (name, text, x0, y0, x1, y1, fontsize, color, bgColor, hColor, hbgColor, action, toggle, unToggleFn) {
+    this.buttonManager.addButton(new Button(name, text, x0, y0, x1, y1, fontsize, color, bgColor, hColor, hbgColor, action,toggle, unToggleFn));
 }
 Engine.prototype.addButtonJSON = function (json) {
     let buttons = JSON.parse(json);
