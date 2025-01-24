945437fe9d9358880507046cc057412b79c92058
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-23 23:35:08 -0400

    Added the horizontal panel, and testing for it.

--- buttonpanel.js
+++ buttonpanel.js
@@ -10,18 +10,16 @@ const VerticalButtonPanel = function (name, colors, x, y, width, buttonHeight, f
     this.width = width;
     this.height = undefined;
     this.buttonHeight = buttonHeight;
-    this.fontSize = fontSize; 
+    this.fontSize = fontSize;
     this.outerBorder = outerBorder; //distance between panel left/right edge and button also vertical spacing between buttons.
-    this.radioGroup = (radioGroup)? radioGroup : null;
+    this.radioGroup = (radioGroup) ? radioGroup : null;
 }
 VerticalButtonPanel.prototype.add = function (buttonDefinitions) {
     //buttonDefinitions is an array of {name, text, action} objects.
     //They'll be drawn in the order they appear in the array, top to bottom
-   
+
     let y = this.y + this.outerBorder;
-    console.log (` y${y} this.y ${this.y} bh ${this.buttonHeight} outerborder ${this.outerBorder}`);
     buttonDefinitions.forEach(def => {
-        console.log (def);
         //name, text, x0, y0, x1, y1, fontsize, buttonColors, actionFn
         let button = new Button(def.name, def.text,
             this.x + this.outerBorder,
@@ -29,14 +27,39 @@ VerticalButtonPanel.prototype.add = function (buttonDefinitions) {
             this.x + this.width - this.outerBorder * 2,
             y + this.buttonHeight,
             this.fontSize, this.colors, def.actionFn);
-        console.log (`def.actionFn ${def.actionFn}`);
-        this.buttons.push (button);
+        this.buttons.push(button);
         y += this.buttonHeight + this.outerBorder;
     });
-    this.height = y + this.y+this.outerBorder*2;
-    console.log (`this.height ${this.height}`);
-}   
+    this.height = y + this.y + this.outerBorder * 2;
+}
 
-//TODO:  Write a test!
-
-//TODO: Horizontal panel..
+const HorizontalButtonPanel = function (name, colors, x, y, height, buttonWidth, fontSize, outerBorder, radioGroup) {
+    this.name = name;
+    this.buttons = [];
+    this.x = x;  //uppper left hand corner...
+    this.y = y;
+    this.colors = colors;
+    this.width = undefined;
+    this.height = height;
+    this.buttonWidth = buttonWidth;
+    this.fontSize = fontSize;
+    this.outerBorder = outerBorder; //distance between panel left/right edge and button also vertical spacing between buttons.
+    this.radioGroup = (radioGroup) ? radioGroup : null;
+}
+HorizontalButtonPanel.prototype.add = function (buttonDefinitions) {
+    let x = this.x + this.outerBorder;
+    buttonDefinitions.forEach(def => {
+        //name, text, x0, y0, x1, y1, fontsize, buttonColors, actionFn
+        let button = new Button(def.name, def.text,
+            x,
+            this.y,
+            x + this.buttonWidth + this.outerBorder,
+            this.y + this.height,
+            this.fontSize, this.colors, def.actionFn
+        );
+        console.log (`x ${x} this.buttonWidth`);
+        this.buttons.push(button);
+        x += this.buttonWidth + this.outerBorder;
+    });
+    this.width = x + this.y + this.outerBorder * 2;
+}
