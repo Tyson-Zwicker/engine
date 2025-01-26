824de9823649caa4288444f49854d681ed1145a3
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-25 23:00:20 -0400

    fixed colors being the opposite of what they were supposed to, also now the border is being drawn, and simplified how the colors are stored in button.js

--- button.js
+++ button.js
@@ -12,10 +12,7 @@ const Button = function (name, text, x0, y0, x1, y1, fontsize, buttonColors, act
     this.y0 = y0;
     this.x1 = x1;
     this.y1 = y1;
-    this.color = buttonColors.color;
-    this.bgColor = buttonColors.bgColor; 
-    this.hColor = buttonColors.hoverColor;
-    this.hbgColor = buttonColors.hoverBackground;
+    this.colors = buttonColors;
     this.width = x1 - x0;
     this.height = y1 - y0;
     this.fontsize = fontsize;
@@ -34,24 +31,28 @@ Button.prototype.selectColor = function () {
     let text = undefined;
     let bg = undefined;
     if (this.manager.toggled.has(this)){
-        border = this.bgColor;
+        //toggled same as pressed
+        border = this.colors.hoverBackground;
         text = border;
-        bg = this.color;
+        bg = this.colors.hoverColor;
     } else {
         if (this.manager.hovered === this) {
             if (this.manager.pressed === this) {
-                border = this.bgColor;
+                //pressed
+                border = this.colors.hoverBackground;
                 text = border;
-                bg = this.color;
+                bg = this.colors.hoverColor;
             } else {
-                border = this.color;
+                //hovered
+                border = this.colors.hoverColor;
                 text = border;
-                bg = this.bgColor;
+                bg = this.colors.hoverBackground;
             }
         } else {
-            border = this.hColor;
+            //normal
+            border = this.colors.color;
             text = border;
-            bg = this.hbgColor;
+            bg = this.colors.bgColor;
         }
     }
     return { 'border': border, 'text': text, 'bg': bg };
@@ -61,6 +62,6 @@ Button.prototype.draw = function () {
     let mx = this.x0 + (this.x1 - this.x0) / 2;
     let my = this.y0 + (this.y1 - this.y0) / 2;
     drawBox(this.x0, this.y0, this.x1, this.y1, colors.bg, true);
-    drawBox(this.x0, this.y0, this.x1, this.y1, colors.bg, false, 3);
+    drawBox(this.x0, this.y0, this.x1, this.y1, colors.border, false, 1);
     drawTextCenter(mx, my, this.text, this.fontsize, colors.text);
 }
