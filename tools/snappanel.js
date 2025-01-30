98acc42287542ef704ac957f20b50f7ae81b58c1
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-01-29 21:40:06 -0400

    finally making progress on this thing.

--- tools/snappanel.js
+++ tools/snappanel.js
@@ -3,67 +3,125 @@ const SnapPanel = function (x0, y0, x1, y1) {
     this.y0 = y0;
     this.x1 = x1;
     this.y1 = y1;
-    this.snap = undefined;
-    this.snapPoints = undefined;
     this.hSym = false;
     this.vSym = false;
+    this.hoveredPoint = null;
+    this.pressedPoint = null;
     //for these think (x,y)<-->(a,b)  //These are undefined until span is defined;
     this.a0 = undefined;//left most x for grid line (depends on snap)
     this.b0 = undefined;//upper most y for grid line (depends on snap)
     this.a1 = undefined//right most x for gridline (depends on snap)
     this.b1 = undefined;//lower most y for grid line (depends on snap)
+    this.snap = undefined;
+    this.snapPoints = undefined;
+    this.numXLines = undefined;
+    this.numYLines = undefined;
+    this.getNearestPoint = function (point) {        
+        if (snap) {
+            let shortestDistance = undefined;
+            let nearestPoint = undefined;
+            for (let i = 0; i < this.numXLines; i++) {
+                for (let j = 0; j < this.numYLines; j++) {
+                    let distance = distance (this.snapPoints [x][y], point);
+                    if (!shortestDistance || shortestDistance>distance){
+                        shortestDistance = distance;
+                        nearestPoint = this.snapPoint[i][j];
+                    }
+                }
+            }
+            return nearestPoint;
+        } 
+        throw new Error ('cannot calculate distance: snap level not declared');
+    }
 }
 SnapPanel.prototype.setSnap = function (snap) {
-    this.snap = snap;    
+    this.snap = snap;
     let cx = (this.x1 - this.x0) / 2;
     let cy = (this.y1 - this.y0) / 2;
-    let numxlines = Math.trunc((this.x1 - this.x0) / snap);
-    let numylines = Math.trunc ((this.y1 - this.y0) / snap);
-    console.log (`numxlines${numxlines}`);
-    console.log (`numylines${numylines}`);
-    this.a0 = cx - numxlines * snap;
-    this.b0 = cy - numylines * snap;
-    this.a1 = cx + numxlines * snap;
-    this.b1 = cy + numylines * snap;
+    this.numXLines = Math.trunc((this.x1 - this.x0) / snap);
+    this.numYLines = Math.trunc((this.y1 - this.y0) / snap);
+    this.a0 = cx - this.numXLines * snap;
+    this.b0 = cy - this.numYLines * snap;
+    this.a1 = cx + this.numXLines * snap;
+    this.b1 = cy + this.numYLines * snap;
     this.snapPoints = [];
-    let i=0;j=0;
-    for (let x = this.a0; x < this.a1; x += snap) {        
-        this.snapPoints.push ([]);
+    let i = 0; j = 0;
+    for (let x = this.a0; x < this.a1; x += snap) {
+        this.snapPoints.push([]);
         for (let y = this.b0; y < this.b1; y += snap) {
-            if (!this.snapPoints[i]){
-                this.snapPoints[i].push ([]);
+            if (!this.snapPoints[i]) {
+                this.snapPoints[i].push([]);
             }
-            this.snapPoints[i][j] = {x,y};
+            this.snapPoints[i][j] = { x, y };
             j++;
         }
-        i++; j=0;
+        i++; j = 0;
     }
-    
+
 }
 SnapPanel.prototype.setSymmetry = function (horizontal, vertical) {
     this.vSym = vertical;
     this.hSym = horizontal;
 }
-
-SnapPanel.prototype.hovered = function (x,y){
-    //How do you iterate over an IArray? 
-}
-SnapPanel.prototype.clicked = function (x, y) {
-    if (!snap) {
-        throw Error('Cannot draw snap Panel: Snap level not declared.');
+//set hoveredPoint, used internally to highlight that point when drawn.
+SnapPanel.prototype.reactToHover = function (point) {
+    if (this.snap) {
+        if (bounded (point, {x0,y0,x1,y1})){
+            this.hoveredPoint = this.getNearestPoint (point)
+        }else{
+            this.hoveredPoint = null;
+        }
     } else {
+        throw Error('snappanel: Snap level not declared.');
+    }
+}
+//set pressPoint, used internally to highlight that point when drawn 
+//AND determine if its been clicked once the mouse comes back up.
+SnapPanel.prototype.reactToMouseDown = function (point) {
+    if (this.snap) {
+        if (bounded (point, {x0,y0,x1,y1})){
+            this.pressedPoint = this.getNearestPoint (point);
+        }else{
+            this.hoveredPoint = null;
+        }
+    } else {
+        throw Error('snappanel: Snap level not declared.');
+    }
+}
+//returns the gridPoint that was clicked or null if no point was clicked.
+SnapPanel.prototype.reactToMouseUp = function (point) {
+    if (this.snap) {
+        if (bounded (point, {x0,y0,x1,y1})){
+            let nearestPoint = this.getNearestPoint (point);
+            if (nearestPoint.x === this.pressedPoint.x && nearestPoint.y === this.pressedPoint.y){
+                return  nearestPoint;
+            }else{
+                this.pressedPoint = null;
+                return null;
+            }
+        }else{
+            this.pressedPoint = null
+            return null;
+        }
 
+    } else {
+        throw Error('snappanel: Snap level not declared.');
     }
 }
 SnapPanel.prototype.draw = function () {
     if (!snap) {
-        throw Error('Cannot draw snap Panel: Snap level not declared.');
+        throw Error('Cannot draw snappanel: Snap level not declared.');
     } else {
+        //Draw the lines..
         for (let x = this.sx; x < this.ex; x += this.snap) {
             drawLine(x, this.y0, x, this.y1, '#777', 1);
         }
         for (let y = this.sy; y < this.ey; y += this.snap) {
             drawLine(this.x0, y, this.x1, y, '#777', 1);
         }
+        //draw symetry lines along centeral gridlines..
+
+        //draw small box at each gridPoint..
+        //highlight hovered and highlight pressed with different color..
     }
 }
--- tools/spritemaker.js
+++ tools/spritemaker.js
@@ -2,7 +2,7 @@ const spriteMaker = {
     vSym: false,
     hSym: false,
     snapSize: 1,
-    points: new IArray(),
+    points: [],
     action: 'add' //valid values are 'add' and 'remove'
 }
 const setVSym = function () { spriteMaker.vSym = true; }
@@ -11,8 +11,15 @@ const unsetVSym = function () { spriteMaker.hSym = true; }
 const unsetHSym = function () { spriteMaker.hSym = false; }
 const setSnap = function (param) { spriteMaker.snapSize = param; }
 const setActionAdd = function (){ spriteMaker.action ='add'}
-const setActionRemove = function (){spriteMaker.action ='remove'}
-
+const setActionRemoveLast = function (){
+    spriteMaker.points.pop();
+}
+const setActionRemoveFirst = function (){
+    spriteMaker.points.splice(0,1);
+}
+const setActionClearAll = function (){
+    spriteMaker.points = [];
+}
 const engine = new Engine();
 const buttonColors = new ButtonColors('#fff', '#0af', '#ff0', '#b0f');
 
@@ -25,8 +32,8 @@ const symPanel = new VerticalButtonPanel('symPanel', buttonColors, '#008', 10, 5
 symPanel.add(symButtons);
 engine.addPanel(symPanel);
 
-const snapPanel = new HorizontalButtonPanel('snapPanel', buttonColors, '#420', 10, 130, 30, 20, .8, 5, true);
-const snapButtons = [
+const snapLevelPanel = new HorizontalButtonPanel('snapPanel', buttonColors, '#420', 10, 130, 30, 20, .8, 5, true);
+const snapLevelButtons = [
     { name: 'snap1', text: '1', actionFn: setSnap, actionParam:1 },
     { name: 'snap2', text: '2', actionFn: setSnap, actionParam:2 },
     { name: 'snap5', text: '5', actionFn: setSnap, actionParam:5 },
@@ -34,16 +41,15 @@ const snapButtons = [
     { name: 'snap25', text: '25', actionFn: setSnap, actionParam:25 },
     { name: 'snap50', text: '50', actionFn: setSnap, actionParam:50 },
 ];
-snapPanel.add(snapButtons);
-engine.addPanel(snapPanel);
+snapLevelPanel.add(snapLevelButtons);
+engine.addPanel(snapLevelPanel);
 
-//TODO:  Add an (optional) title/label to the panels.
-
-const actionPanel = new HorizontalButtonPanel('actionPanel', buttonColors, '#008',10,175,30,70,.8,5,true);
+const actionPanel = new VerticalButtonPanel('actionPanel', buttonColors, '#008',10,175,158,25,.8,5,true);
 const actionButtons = [
-    { name: 'add', text: 'Add', actionFn: setActionAdd },
-    { name: 'remove', text: 'Remove', actionFn: setActionRemove },
+    { name: 'addpoint', text: 'Add', actionFn: setActionAdd },
+    { name: 'rmlast', text: 'Remove Last', actionFn: setActionRemoveLast },
+    { name: 'rmfirst', text: 'Remove First', actionFn: setActionRemoveFirst },
+    { name: 'clear', text: 'Clear All', actionFn: setActionClearAll },
 ]
 actionPanel.add (actionButtons);
 engine.addPanel (actionPanel);
-//TODO: I want to be able to set the toggle state in the button def.
